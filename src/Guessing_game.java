05c4ff2ed5e285fa93934ab585a75670e4873eae
Thi Bich Loan Chung <bichloan.1032170@gmail.com>
2021-03-22 12:10:56 +0100

    initialize an array list of 10 ints and shuffle it

--- src/Guessing_game.java
+++ src/Guessing_game.java
@@ -1,8 +1,16 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Guessing_game {
 
 	public static void main(String[] args) {
-		
+		ArrayList<Integer> arrOfInt = new ArrayList<Integer>();
+		for (int i = 0; i < 10; i++) {
+			arrOfInt.add(i+1);
+		}
+		System.out.println(arrOfInt);
+		Collections.shuffle(arrOfInt);
+		System.out.println(arrOfInt);
 	}
 
 }
